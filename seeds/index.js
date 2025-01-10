e794ce2f7b3c2302905e658867b9fab5b6c4f78a
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-10 11:07:42 +0700

    NodeCamp [Campgrounds CRUD] - Seeding Campgrounds

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+const dbUrl = process.env.DB_URL || 'mongodb://172.21.176.1:27017/database_name';
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    // const camp = new Campground({ title: 'New Seed', price: 100, description: 'cheap camping!', location: 'Mount Pangrango' });
+    // await camp.save();
+    for(let i = 0; i < 300; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        });
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
