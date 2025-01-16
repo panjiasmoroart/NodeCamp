001877a6bd5fa6cb8ffdc6f16b74fe55fedd6c29
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-16 15:24:47 +0700

    NodeCamp [Adding The Reviews Model] - Validating Reviews

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
@@ -40,6 +40,17 @@ const validateCampground = (req, res, next) => {
         next();
     }
 }
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 
 app.get('/', (req, res) => {
     res.render("home");
@@ -83,7 +94,7 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     res.redirect(`/campgrounds`);
 }));
 
-app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
