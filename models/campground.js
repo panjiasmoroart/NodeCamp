15c96b5015004d7cc2d8c656c04f2ec5927f62ac
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-16 14:14:16 +0700

    NodeCamp [Adding The Reviews Model] - Defining The Review Model

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -7,6 +8,12 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
