15c882cc458522f5d8efadb8da83e7a2271da95e
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-18 18:11:09 +0700

    NodeCamp [Restructuring & Flash] - Breaking Out Review Routes

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 const dbUrl = process.env.DB_URL || 'mongodb://172.21.176.1:27017/database_name';
 
@@ -32,37 +30,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
 app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render("home");
 });
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await Promise.all([review.save(), campground.save()]);
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    // $pull: Operator MongoDB untuk menghapus elemen dari array
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
 
 // more errors 
 app.all(/(.*)/, (req, res, next) => {
