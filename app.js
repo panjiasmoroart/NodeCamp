feaba9770cb3cad55f7d432d7650ff0862421942
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-18 22:21:42 +0700

    NodeCamp [Restructuring & Flash] - Setting Up Flash

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-app.use(express.static(path.join(__dirname, 'public')))
 const ExpressError = require('./utils/ExpressError');
+const session = require('express-session');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -30,6 +30,20 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
@@ -38,9 +52,8 @@ app.get('/', (req, res) => {
     res.render("home");
 });
 
-// more errors 
+
 app.all(/(.*)/, (req, res, next) => {
-    // res.send("404!!!!");
     next(new ExpressError('page not found', 404));
 });
 
