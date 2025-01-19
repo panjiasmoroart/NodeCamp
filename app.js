7d057353958e97a4c0433ea14edd7aadf8b56641
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-19 08:01:32 +0700

    NodeCamp [Restructuring & Flash] - Configuring Session

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configuring session
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false,
