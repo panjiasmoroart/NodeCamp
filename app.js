5c1aee90098e1c359eeb1376d122347f513bd6ed
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-20 22:17:29 +0700

    NodeCamp [Adding In Authentication] - Configuring Passport

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -48,10 +51,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+});
+
+app.get('/fakeUser', async(req, res) => {
+    const user = new User({ email: 'panjiasmoro@gmail.com', username: 'panjiasmoro' });
+    const newUser = await User.register(user, 'panjiasmoro');
+    res.send(newUser);
 });
 
 app.use('/campgrounds', campgrounds);
