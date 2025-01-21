bfb11c289c97c0a3f05e670bd74093d2d2049a5a
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-21 21:41:55 +0700

    NodeCamp [Adding In Authentication] - currentUser Helper

--- middleware.js
+++ middleware.js
@@ -1,4 +1,12 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    // console.log("REQ USER...", req.user);
+    // REQ USER... {
+    //     _id: new ObjectId('678e682a723a009b1d71491a'),
+    //     email: 'usertesting@gmail.com',
+    //     username: 'usertesting',
+    //     __v: 0
+    // }
+
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
