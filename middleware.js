f88fef4cccdaaa8a7a7762943ab961e134a4c449
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-22 14:46:25 +0700

    NodeCamp [Adding In Authentication] - ReturnTo Behavior

--- middleware.js
+++ middleware.js
@@ -8,8 +8,28 @@ module.exports.isLoggedIn = (req, res, next) => {
     // }
 
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl; 
+        // console.log('req.path,  req.originalUrl >>>', req.path, req.originalUrl);
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
     next();
 }
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+// req.path,  req.originalUrl >>> /new /campgrounds/new
+// Session {
+//   cookie: {
+//     path: '/',
+//     _expires: 2025-01-29T07:36:51.817Z,
+//     originalMaxAge: 604799999,
+//     httpOnly: true
+//   },
+//   flash: { error: [ 'You must be signed in first!' ] },
+//   returnTo: '/campgrounds/new'
+// }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -31,9 +32,11 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res, next) => {
