237deac4c41b5981fa1bc3cebc4b06e22ef357f0
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-22 16:40:47 +0700

    NodeCamp [Basic Authorization] - Showing and Hiding Edit or Delete

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -37,7 +37,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async(req, res, next
 router.get('/:id', catchAsync(async(req, res) => {
     // const campground = await Campground.findById(req.params.id);
     const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
-    console.log(campground);
+    // console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
