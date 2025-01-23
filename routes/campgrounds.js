ac6b2b67c75953853ca18ebece96ff0a2cf59732
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-23 18:01:51 +0700

    NodeCamp [Image Upload] - The Multer Middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files)
+        res.send("IT Worked");
+        // res.status(201).send({ body: req.body, file: req.file });
+    });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
