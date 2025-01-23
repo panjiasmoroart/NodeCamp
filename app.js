631d06e9c3c0a12dc2f170bd890f8e9c409c2e44
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-23 21:52:52 +0700

    NodeCamp [Image Upload] - Environment Variables with dotenv

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
+// console.log(process.env.CLOUDINARY_CLOUD_NAME);
+
 const express = require('express');
 const app = express();
 const path = require('path');
