48d95be0bd8e63c4ffe5d85f66f62cd9977819c9
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-25 09:51:06 +0700

    NodeCamp [Image Upload] - Adding a Thumbnail Virtual Property

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,10 @@ const ImageSchema = new Schema({
     filename: String
 });
 
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
