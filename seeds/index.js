1bfe4275d419608280cdd320a0e5587ed912f825
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-26 10:27:25 +0700

    NodeCamp [Adding Maps] - Fixing Our Seeds Bug

--- seeds/index.js
+++ seeds/index.js
@@ -27,12 +27,20 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const randomPrice = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            // your user id in mongo
             author: '678e682a723a009b1d71491a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             // image: `https://picsum.photos/400?random=${Math.random()}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price: randomPrice,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [
                 {
                   url: 'https://res.cloudinary.com/dh1e1yjer/image/upload/v1737706396/NodeCamp/hemfedgq5twogri2wwbl.jpg',
