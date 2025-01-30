752061f97e320192903d28e85df1d3fa42a4eb54
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-30 16:14:52 +0700

    NodeCamp [Common Security Issues] - Hiding Errors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
+
+// for production 
+require('dotenv').config();
 
 // console.log(process.env.CLOUDINARY_CLOUD_NAME);
 
